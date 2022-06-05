961f5b34eb3e16ff772b399f8bee07187a2184d3
Diego <75797588+diego4351r@users.noreply.github.com>
2022-06-05 09:39:03 +0200

    React 18 updated, fixed eslint and build bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'standard', 'eslint-config-prettier', 'plugin:jest-dom/recommended', 'plugin:testing-library/react', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'standard', 'prettier', 'plugin:jest-dom/recommended', 'plugin:testing-library/react', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,7 +10,7 @@ export default function AppRoutes() {
 	return (
 		<Suspense fallback={<span>Loading...</span>}>
 			<Router>
-
+				<h1>hola k ase</h1>
 			</Router>
 		</Suspense>
 	);
