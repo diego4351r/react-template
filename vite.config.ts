26668c660a62ddba7040e6f3d93dd8ce61164cdd
Diego <75797588+diego4351r@users.noreply.github.com>
2022-06-07 12:19:37 +0200

    Removed sassdts and fix sass bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import sassDts from "vite-plugin-sass-dts";
-import * as path from 'path';
 
 export default defineConfig(() => {
 	return {
 		plugins: [
 			tsconfigPaths(),
-			sassDts({
-				enabledMode: ['production'],
-				global: {
-					generate: true,
-					outFile: path.resolve(__dirname, './src/style.d.ts')
-				}
-			}),
 			react()
 		],
 		server: {
@@ -28,16 +19,7 @@ export default defineConfig(() => {
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: `@use "@/styles" as *;`,
-					importer(...args) {
-						if (args[0] !== '@/styles') {
-							return;
-						}
-
-						return {
-							file: `${path.resolve(__dirname, './src/assets/styles')}`
-						};
-					}
+					additionalData: `@use "./src/assets/styles" as *;`,
 				}
 			}
 		}
